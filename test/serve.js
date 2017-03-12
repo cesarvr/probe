ef1e65b45cb4dca88e11c5004b90f90926971ea0
cesar <cesarv01@yahoo.com>
2017-03-12 07:38:31 +0000

    adding git repo metadata

--- test/serve.js
+++ test/serve.js
@@ -1,19 +1,45 @@
 'use strict';
 
 var express = require('express')
+
 var http = require('http')
 
 var app = express();
 
 var probe = require('../probe')
+
 app.use(probe());
+
+
+
+var fibonacci = function(n) {
+    return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+
 
 app.get('/fast', function(req, res, next) {
     res.send('fast');
 });
 
+app.get('/fib', function(req, res, next) {
+
+    var n = Math.floor(Math.random() * 40) + 1;
+
+    res.send({
+        fibonacci: {
+            'compute:': fibonacci(n),
+            'iteration': n
+        }
+    });
+
+});
+
+
 app.get('/slow', function(req, res, next) {
+
     var _time = Math.floor(Math.random() * 1000) + 1;
+
     setTimeout(() => {
         res.send({
             slow: {
@@ -21,6 +47,7 @@ app.get('/slow', function(req, res, next) {
             }
         });
     }, _time)
+
 });
 
 var httpServer = http.createServer(app);
