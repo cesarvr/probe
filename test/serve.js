668e665c3e2332f669eda672322d3d74703d6726
cesar <cesarv01@yahoo.com>
2017-05-01 13:37:29 +0100

    adding more information to test

--- test/serve.js
+++ test/serve.js
@@ -8,15 +8,19 @@ var app = express();
 var probe = require('../probe')
 app.use(probe());
 
-app.get('/fast', function(req, res, next){
-  res.send('fast');
+app.get('/fast', function(req, res, next) {
+    res.send('fast');
 });
 
-app.get('/slow', function(req, res, next){
-  var _time = Math.floor(Math.random() * 5000) + 1;
-  setTimeout(()=>{
-    res.send( 'sloooowww ' + _time );
-  },_time)
+app.get('/slow', function(req, res, next) {
+    var _time = Math.floor(Math.random() * 1000) + 1;
+    setTimeout(() => {
+        res.send({
+            slow: {
+                'time:': _time
+            }
+        });
+    }, _time)
 });
 
 var httpServer = http.createServer(app);
--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ sleep 4
 
 echo "testing...."
 
-for ((i=1;i<=5;i++)); do
+for ((i=1;i<=10;i++)); do
 
 echo "\n"
 curl "http://localhost:8080/slow" -H "Pragma: no-cache" -H "Accept-Encoding: gzip, deflate, sdch, br" -H "Accept-Language: en-US,en;q=0.8,es;q=0.6" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Cache-Control: no-cache" -H "Cookie: mock_uuid=E1D2BDB93B6B494F912E90CD5C699ECC; _ga=GA1.1.1437560514.1444121988; token={%22__v%22:0%2C%22userId%22:%22BMUser03%22%2C%22name%22:%22Bmuser03%22%2C%22admin%22:true%2C%22sessionToken%22:%22FSSESSION=cf6fcd9f4f970a37dbaeb5188173fbd5c2ed6e825e420f0ed8392d4e04459e76ad74558dfa3c83b1%22%2C%22time%22:1448974545636%2C%22_id%22:%22565d98d1d7fc4221616b30b6%22}" -H "Connection: keep-alive" --compressed
@@ -20,4 +20,5 @@ echo "\n"
 done
 
 #destroy the server
+echo "\n killing node process"
 pgrep node | xargs sudo kill
